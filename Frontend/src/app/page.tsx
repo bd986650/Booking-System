433ff6cd97c6e5403cac0059b196d07a64603fa0
bd986650 <bd986650@gmail.com>
2025-10-03 19:22:32 +0700

    with folder Frontend andcleaned history

---
+++ Frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+// import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <h1 className="text-center text-amber-50 bg-gray-900 my-20 p-20 animate-bounce">
+      Booking System
+    </h1>
+  );
+}
